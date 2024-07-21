a962dc661c8c5723ffdf1dd443bb854111662463
sofia_almroth <almrothsogfia@gmail.com>
2024-07-21 13:09:29 +0200

    changed to book

---
+++ prisma/migrations/20240721110632_changed_back_to_book/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [NOVEL] on the enum `BookType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "BookType_new" AS ENUM ('DVD', 'BOOK', 'AUDIOBOOK', 'ENCYCLOPEDIA');
+ALTER TABLE "LibraryItem" ALTER COLUMN "type" TYPE "BookType_new" USING ("type"::text::"BookType_new");
+ALTER TYPE "BookType" RENAME TO "BookType_old";
+ALTER TYPE "BookType_new" RENAME TO "BookType";
+DROP TYPE "BookType_old";
+COMMIT;
